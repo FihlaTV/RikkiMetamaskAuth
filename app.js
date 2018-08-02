6d1e362560f642ee339b4c301e19bb1966c2fd5c
Rikki407 <rishablamba407@gmail.com>
2018-08-03 00:01:03 +0530

    page setup

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const express = require('express');
-const  app = express();
+const express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    passport = require('passport'),
+    mongoose = require('mongoose');
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-    res.render(index)
+    res.render('index');
+});
+
+app.post('/register', (req, res)=>{
+    
 })
 
 app.listen(process.env.PORT || 4000);
